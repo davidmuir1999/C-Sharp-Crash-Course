0ac2a8db989465d87aa003073ffea65c3247cdf8
davidmuir1999 <david.bme.muir2017@gmail.com>
2023-08-21 22:46:24 +0100

    Code to print to the console

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program //a class is essentially a container where we can insert code
+    {
+        static void Main(string[] args) // this is a method/function.
+        //Anything within the main function will be executes by the computer
+        {
+            Console.WriteLine("Hello World"); // must end each line in C# with ";"
+            Console.ReadLine(); // allows console to stay
+        }
+    }
+}
