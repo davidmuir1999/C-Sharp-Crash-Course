431f63161ca3e7fba23e3f9591d33a595408c62a
davidmuir1999 <david.bme.muir2017@gmail.com>
2023-08-23 20:05:03 +0100

    Math Methods

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -11,11 +11,7 @@ namespace Giraffe
         static void Main(string[] args) // this is a method/function.
         //Anything within the main function will be executes by the computer
         {
-          string phrase = "David Muir"; //string is denoted with string variable and double quotations
-          char grade = 'A'; //char is denoted by a single letter and single quotations
-          int age = 24; //int is denoted by a rounded number
-          double gpa = 3.2; // similar to decimal data type but less accurate, others include decimal and float
-          bool  isMale = true; //boolean 
+            Console.WriteLine(Math.Abs(40)); // when using number methods, use "Math"
 
             Console.ReadLine(); // allows console to stay
         }
