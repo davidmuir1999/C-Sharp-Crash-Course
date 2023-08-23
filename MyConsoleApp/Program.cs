77c025124634fe275ac05d9705b810677ff8088a
davidmuir1999 <david.bme.muir2017@gmail.com>
2023-08-23 19:37:16 +0100

    Data Types

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -11,13 +11,11 @@ namespace Giraffe
         static void Main(string[] args) // this is a method/function.
         //Anything within the main function will be executes by the computer
         {
-            string personName = "David";
-            int personAge = 24;
-
-            Console.WriteLine("There once was a man name is " + personName); 
-            Console.WriteLine("He was " + personAge + " years old"); //concatenating a string
-            Console.WriteLine("He really like the name " + personName);
-            Console.WriteLine("But didn't like being " + personAge);
+          string phrase = "David Muir"; //string is denoted with string variable and double quotations
+          char grade = 'A'; //char is denoted by a single letter and single quotations
+          int age = 24; //int is denoted by a rounded number
+          double gpa = 3.2; // similar to decimal data type but less accurate, others include decimal and float
+          bool  isMale = true; //boolean 
 
             Console.ReadLine(); // allows console to stay
         }
